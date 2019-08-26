9b04052a01bdb008c4fec8f412210322ea60d840
mingminghe <mingminghe@ebnew.com>
2019-08-26 14:29:49 +0800

    Number 格式，增加log

--- javascript/oms-match-text/match.js
+++ javascript/oms-match-text/match.js
@@ -18,22 +18,24 @@ const main = {
     },
     // 读入 oms 模板内容
     readOmsFile() {
-        return new Promise((resolve, reject)=>{
+        return new Promise((resolve, reject) => {
             fs.readFile(omsFile, function (err, data) {
                 if (err) {
                     return console.error(err);
                 }
                 let _omsData = JSON.parse(data.toString('UTF8'));
-                main.matchName(_omsData.typeData);
+                main.matchName(_omsData);
                 resolve();
             })
         })
     },
     // 读入列宽配置文件
     readConfigFile() {
-        return new Promise((resolve, reject)=>{
+        return new Promise((resolve, reject) => {
             fs.readFile(configFile, function (err, data) {
-                if (err) { return console.error(err); }
+                if (err) {
+                    return console.error(err);
+                }
                 let _tempArr = data.toString('UTF8').split('\r\n');
                 let _obj = {};
                 _tempArr.map(val => {
@@ -49,24 +51,31 @@ const main = {
     },
     // 双循环匹配 title
     matchName(_data) {
-        for (let key in _data) {
+        for (let key in _data.typeData) {
             for (let val in main.params.configFile) {
-                if (_data[key].title === val) {
-                    _data[key].width = main.params.configFile[val];
+                if (_data.typeData[key].title === val) {
+                    if (Number(_data.typeData[key].width) !== Number(main.params.configFile[val])) {
+                        console.log('源: ' + _data.typeData[key].title + '-' + _data.typeData[key].width + '<=====>' + '目标: ' + main.params.configFile[val])
+                    } else {
+                        console.log('源: ' + _data.typeData[key].title + '-' + _data.typeData[key].width + '****************' + '目标: ' + main.params.configFile[val])
+                    }
+                    _data.typeData[key].width = Number(main.params.configFile[val]);
                 }
             }
         }
 
         main.writeText(_data);
-        
     },
     // 写入文件
     writeText(_array) {
         // 格式化一下
-        let _temp = jsBeautify(JSON.stringify(_array), { indent_size: 2, space_in_empty_paren: true });
+        let _temp = jsBeautify(JSON.stringify(_array), {
+            indent_size: 2,
+            space_in_empty_paren: true
+        });
         fs.writeFile(outFile, _temp, {
             encoding: 'utf8'
-        }, function(err) {
+        }, function (err) {
             if (err) throw err;
             console.log(outFile + ' 生成成功！');
         })
